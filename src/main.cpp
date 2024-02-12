31b10368cffc9ebb89f2ee7ae4de2ca8b1929881
Chance REYES <chance.reyes@rutgers.edu>
2024-02-12 15:33:20 -0500

    added new pins, trying to control windows volume thru BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <Arduino.h>
 #include "CapacitiveSensor.h"
+#include "BLEDevice.h"
+#include "BLEHIDDevice.h"
+#include "HIDTypes.h"
+#include "HIDKeyboardTypes.h"
 
 #define highRes0 4 //High resistor value goes out of this GPIO
+#define highRes1 5
+
 #define senseRes0 3 // Low resistor goes into this GPIO
 #define senseRes1 6
 #define meanCount 11 // puts this many readings into a list before averaging
-#define ledPin D3     // should dim based on the position of the slider
 
-CapacitiveSensor   cap4_3 = CapacitiveSensor(highRes0, senseRes0);  
-CapacitiveSensor   cap4_6 = CapacitiveSensor(highRes0, senseRes1);
+//random things to make the esp32 control
+#define ledPin D3     // should dim based on the position of the slider
+#define US_KEYBOARD 1 // trying to control volume here
+
+//hid definitions
+#define KEY_VOLUP 0x80
+#define KEY_VOLDOWN 0x81
+
+
+
+
+
+
+
+CapacitiveSensor   cap0 = CapacitiveSensor(highRes0, senseRes0);  
+CapacitiveSensor   cap1 = CapacitiveSensor(highRes0, senseRes1);
 
 long reading0;
 long reading1; 
@@ -18,9 +37,26 @@ double sliderValue;
 long output;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void setup() {
-  cap4_3.set_CS_AutocaL_Millis(0xFFFFFFFF);
-  cap4_6.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  cap0.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  cap1.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
 }
@@ -30,20 +66,27 @@ void loop() {
   meanNum1 = 0;
 
   for(int x = 0; x < meanCount; x++){         
-    reading0 = cap4_3.capacitiveSensor(30);   
-    reading1 = cap4_6.capacitiveSensor(30);
+    reading0 = cap0.capacitiveSensor(30);   
+    reading1 = cap1.capacitiveSensor(30);
 
     meanNum0 =+ reading0;
     meanNum1 =+ reading1;
   }
 
-  meanNum0 = 1+(meanNum0/meanCount);              // averages out meanCount amount of readings to reduce noise
-  meanNum1 = 1+(meanNum1/meanCount);              // adds 1 to not divide by 0
+  meanNum0 = 1+(meanNum0/meanCount); 
 
-  sliderValue = static_cast<double>(meanNum1)/static_cast<double>(meanNum0); //doubles to get decimals when dividing
-  output = map(sliderValue, 0, 11, 0, 255);
+  output = map(meanNum0, 0, 310, 0, 255);
   Serial.println(output);
   analogWrite(ledPin, output);
+
+  
+  
+  
+  
+  delay(100);  
+  
+  //DEBUGGING\\\
+
 
   //Serial.println("");
   //Serial.println("Sensor 1:");
@@ -55,7 +98,4 @@ void loop() {
 
   //Serial.println("");
   //Serial.println(sliderValue);
-  
-  
-  delay(100);  
 }
